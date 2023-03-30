8feb58feba31cec69fe056c34569d7710bec01b2
walgleisson valerio <walgleissonvaleriodev@gmail.com>
2023-03-30 00:43:49 -0300

    resolve requisito 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui!"""
-    raise NotImplementedError
+    students_present = 0
+
+    try:
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                students_present += 1
+        return students_present
+
+    except TypeError:
+        return None
