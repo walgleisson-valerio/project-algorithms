0d141385f71bd58b09ab0fa704e9542de9067902
walgleisson valerio <walgleissonvaleriodev@gmail.com>
2023-03-30 01:13:04 -0300

    resolve requisito 02

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message("senha", "senha")
+
+    with pytest.raises(TypeError):
+        encrypt_message(1, 1)
+    
+    assert encrypt_message("senha", 999) == "ahnes"
+
+    assert encrypt_message("senha", 3) == "nes_ah"
+
+    assert encrypt_message("senha", 2) == "ahn_es"
